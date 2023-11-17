bf3076d06d65a755c74e29a9d6a2a705127f0abd
Esteban <jorgestebanmr@gmail.com>
2023-11-17 10:15:06 -0600

    part2 updated

--- main.py
+++ main.py
@@ -9,6 +9,20 @@ class InitialGraph:
 
     def distance2D(p1, p2):
         return np.sqrt(np.sum((p1 - p2)**2))
+    
+    def distance3D(punto1, punto2):
+        """
+        Calcula la distancia euclidiana entre dos puntos en el espacio tridimensional.
+
+        :param punto1: Tupla con las coordenadas (x, y, z) del primer punto.
+        :param punto2: Tupla con las coordenadas (x, y, z) del segundo punto.
+        :return: La distancia entre los dos puntos.
+        """
+        x1, y1, z1 = punto1
+        x2, y2, z2 = punto2
+
+        distancia = math.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
+        return distancia
 
     def to3D(x, y, z):
         r = math.sqrt(x**2 + y**2 + z**2)
@@ -60,64 +74,50 @@ class InitialGraph:
                     weighted_graph[j][i] = distance
 
         return weighted_graph
+    
 
 class WeightedGraph:
+    directed = True 
+    _vertices = [] # The list of vertices.
+    _adjacency_list = [] 
+
     def __init__(self, directed=False):
         self.directed = directed
-        self.adjacency_list = {}
+        self._vertices = []
+        self._adjacency_list = {}  # Cambiar el nombre de la variable
 
     def vertices(self):
-        return list(self.adjacency_list.keys())
+        return list(self._adjacency_list.keys())  # Cambiar el nombre de la variable
 
     def add_vertex(self, v):
-        if v not in self.adjacency_list:
-            self.adjacency_list[v] = []
+        if v not in self._adjacency_list:
+            self._adjacency_list[v] = []
 
     def add_edge(self, v1, v2, e=0):
-        if v1 not in self.adjacency_list or v2 not in self.adjacency_list:
+        if v1 not in self._adjacency_list or v2 not in self._adjacency_list:
             print("El vertice no existe")
             return
         if not self.directed and v1 == v2:
             print("Error en los vertices")
             return
-        for vertex, weight in self.adjacency_list[v1]:
+        for vertex, weight in self._adjacency_list[v1]:
             if vertex == v2:
                 print(f"El vertice ya existe")
                 return
-        self.adjacency_list[v1].append((v2, e))
+        self._adjacency_list[v1].append((v2, e))
         if not self.directed:
-            self.adjacency_list[v2].append((v1, e))
-    
+            self._adjacency_list[v2].append((v1, e))
+
     def adjacent_vertices(self, v):
-        if v not in self.adjacency_list:
+        if v not in self._adjacency_list:
             print("El vertice no existe")
             return []
-        return self.adjacency_list[v]
-    
-    def floyd_marshall(self, start_vertex, end_vertex):
-        n = len(self.vertices())
-        distance = [[float('inf')] * n for _ in range(n)]
-        next_node = [[-1] * n for _ in range(n)]
+        return self._adjacency_list[v]
 
-        for i in range(n):
-            distance[i][i] = 0
-            for edge in self.adjacency_list[self.vertices()[i]]:
-                distance[i][self.vertices().index(edge[0])] = edge[1]
-                next_node[i][self.vertices().index(edge[0])] = self.vertices().index(edge[0])
-
-        for k in range(n):
-            for i in range(n):
-                for j in range(n):
-                    if distance[i][k] != float('inf') and distance[k][j] != float('inf'):
-                        if distance[i][j] > distance[i][k] + distance[k][j]:
-                            distance[i][j] = distance[i][k] + distance[k][j]
-                            next_node[i][j] = next_node[i][k]
-
-        start_index = self.vertices().index(start_vertex)
-        end_index = self.vertices().index(end_vertex)
-        shortest_distance = distance[start_index][end_index]
-
-        return shortest_distance
+    def print_graph(self):
+        for vertex in self._adjacency_list:
+            for edges in self._adjacency_list[vertex]:
+                print(vertex, " -> ", edges[0], " edge weight: ", edges[1])
     
 class TreeNode:
     def __init__(self, parent, v, c):
@@ -133,7 +133,7 @@ class TreeNode:
             path.insert(0, node.v)
             node = node.parent
         return path
-
+        
     def bfs(graph, v0, vg):
         if v0 not in graph.vertices():
             print("El vertice", v0, "no existe")
@@ -157,6 +157,7 @@ class TreeNode:
                     new_node = TreeNode(node, vertex, cost + node.c)
                     frontier.put(new_node)
 
+        print("No se encontró un camino hasta el nodo", vg)
         return None
 
     def dfs(graph, v0, vg):
@@ -203,49 +204,83 @@ class TreeNode:
                     frontier.put((cost, TreeNode(node, vertex[0], vertex[1] + node.c)))
             explored_set[node.v] = 0
 
+    def floyd_marshall(self, start_vertex, end_vertex):
+        n = len(self.vertices())
+        distance = [[float('inf')] * n for _ in range(n)]
+        next_node = [[-1] * n for _ in range(n)]
+
+        for i in range(n):
+            distance[i][i] = 0
+            for edge in self.adjacency_list[self.vertices()[i]]:
+                distance[i][self.vertices().index(edge[0])] = edge[1]
+                next_node[i][self.vertices().index(edge[0])] = self.vertices().index(edge[0])
+
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if distance[i][k] != float('inf') and distance[k][j] != float('inf'):
+                        if distance[i][j] > distance[i][k] + distance[k][j]:
+                            distance[i][j] = distance[i][k] + distance[k][j]
+                            next_node[i][j] = next_node[i][k]
+
+        start_index = self.vertices().index(start_vertex)
+        end_index = self.vertices().index(end_vertex)
+        shortest_distance = distance[start_index][end_index]
+
+        return shortest_distance
+
 def main():
     matrizLectura = np.loadtxt('Lectura.txt')
     matrizMemoria = np.loadtxt('Memoria.txt')
     matrizOperaciones = np.loadtxt('Operaciones.txt')
 
     InitialGraph.plot(matrizLectura)
-    InitialGraph.plot(matrizMemoria)
-    InitialGraph.plot(matrizOperaciones)
+    #InitialGraph.plot(matrizMemoria)
+    #InitialGraph.plot(matrizOperaciones)
 
     posiciones = {
-    'Fz': np.array([0, 0.71934, 0.694658]),
-    'C3': np.array([-0.71934, 0, 0.694658]),
-    'Cz': np.array([0, 0, 1]),
-    'C4': np.array([0.71934, 0, 0.694658]),
-    'Pz': np.array([0, -0.71934, 0.694658]),
-    'PO7': np.array([-0.587427, -0.808524, -0.0348995]),
-    'Oz': np.array([0, -0.999391, -0.0348995]),
-    'PO8': np.array([0.587427, -0.808524, -0.0348995]),
+        'Fz': (0, 0.71934, 0.694658),
+        'C3': (-0.71934, 0, 0.694658),
+        'Cz': (0, 0, 1),
+        'C4': (0.71934, 0, 0.694658),
+        'Pz': (0, -0.71934, 0.694658),
+        'PO7': (-0.587427, -0.808524, -0.0348995),
+        'Oz': (0, -0.999391, -0.0348995),
+        'PO8': (0.587427, -0.808524, -0.0348995),
     }
 
-    matriz_conectividad = np.array([
-        [0, 0, 1, 0, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0, 0, 0],
-        [1, 1, 0, 1, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 1, 1, 0, 1],
-        [0, 0, 0, 0, 0, 0, 1, 0]
-    ])
-    grafo_ponderado = InitialGraph.toWeigthedMatriz(posiciones, matriz_conectividad)
+    posName = list(posiciones.keys())  
+    indice_a_clave = {str(indice): clave for indice, clave in enumerate(posiciones)}
+    
+    matrizPonderada = InitialGraph.toWeigthedMatriz(posiciones, matrizLectura)
+    graph = WeightedGraph(directed=True)
 
-    # Imprimir la matriz ponderada
-    print("Matriz Ponderada:")
-    print(grafo_ponderado)
+    for i in range(len(matrizPonderada)):
+        graph.add_vertex(posName[i])
+        print(i)
+
+    for i in range(len(matrizPonderada)):
+        for j in range(len(matrizPonderada[i])):
+            if(matrizPonderada[i][j] != 0):
+                graph.add_edge(indice_a_clave[str(i)], indice_a_clave[str(j)], matrizPonderada[i][j])
+
+    '''
+        BFS:
+    '''
+
+    #Fz - PO8
+    res = TreeNode.bfs(graph, 'Fz', 'PO8')
+    print(res)
+
+    graph.print_graph()
+
+    #C3 - Oz
+    res = TreeNode.bfs(graph, 'C3', 'C4')
+    print(res)
+
+    #P07 - C4
+    res = TreeNode.bfs(graph, 'P07', 'C4')
+    print(res)
 
 if __name__ == "__main__":
     main()
-
-import numpy as np
-
-# Definir las posiciones 3D de los electrod
-
-# Función para calcular la distancia euclidiana entre dos puntos 3D
-def distancia_euclidiana(punto1, punto2):
-    return np.linalg.norm(punto1 - punto2)
